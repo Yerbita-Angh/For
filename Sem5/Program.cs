85948722076b22c770f81098e443b621448ef172
Yerbita-Angh <alejandronicolasgarcia01@gmail.com>
2024-09-12 10:06:47 -0500

    for new

--- Sem5/Program.cs
+++ Sem5/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hola, Buen dia");
+string re = "s";
+int suma = 0;
+double cant = 0;
+
+while (re=="s")
+{
+    Console.WriteLine("Desea participar (s/n)");
+    re = Console.ReadLine().ToLower().Trim();
+    if (re == "s")
+    {
+
+        Console.WriteLine("Ingrese edad");
+        int edad = Convert.ToInt32(Console.ReadLine());
+        suma = suma + edad;
+        cant = cant + 2;
+    }
+}
+double prom = suma / cant;
+Console.WriteLine($"El promedio de {cant} edad es {prom}");
